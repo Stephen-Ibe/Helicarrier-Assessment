94cf4f435114b809fc837790f338001c9718ff48
Stephen-Ibe <saintkalson@gmail.com>
2022-07-10 05:24:35 +0100

    configured/integrated apollo client

--- src/index.js
+++ src/index.js
@@ -1,15 +1,43 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './assets/css/index.css';
 import App from './App';
+import './assets/css/index.css';
 import ErrorBoundary from './components/hoc/ErrorBoundary';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        histories: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+      },
+    },
+  },
+});
+
+const client = new ApolloClient({
+  uri: 'http://localhost:9002/graphql',
+  cache,
+});
 
 root.render(
   <React.StrictMode>
     <ErrorBoundary>
-      <App />
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
     </ErrorBoundary>
   </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
