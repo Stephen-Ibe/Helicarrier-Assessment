6417d664d39b771f1b1feda9b50ca2d7b40bbe58
Stephen-Ibe <saintkalson@gmail.com>
2022-07-10 05:12:13 +0100

    filter UI and component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import Filter from './components/blocks/Filter/Filter';
 import Search from './components/blocks/Search/Search';
+import { statusOptions, typeOptions } from './utils/elementOptions';
 
 const App = () => {
   return (
     <div className=' mx-auto w-10/12 sm:w-6/12 h-screen pt-10'>
       <Search />
+      <div className='filter flex flex-row my-7 space-x-4 overflow-x-auto sm:overflow-auto'>
+        <Filter title='status' options={statusOptions} />
+        <Filter title='type' options={typeOptions} />
+      </div>
     </div>
   );
 };
---
+++ src/utils/elementOptions.js
@@ -0,0 +1,21 @@
+export const statusOptions = [
+  {
+    name: 'Successful',
+    value: 'successful',
+  },
+  {
+    name: 'Pending',
+    value: 'pending',
+  },
+  {
+    name: 'Failed',
+    value: 'failed',
+  },
+];
+
+export const typeOptions = [
+  { name: 'Invoice', value: 'invoice' },
+  { name: 'Deposit', value: 'deposit' },
+  { name: 'Payment', value: 'payment' },
+  { name: 'Withdrawal', value: 'withdrawal' },
+];
